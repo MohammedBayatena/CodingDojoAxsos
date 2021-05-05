50a65f362cb93e1961dc2bf66af0d36b7f0da1fe
Mohammed Bayatena <www.cars2012@gmail.com>
2021-05-05 22:18:08 +0300

    Fixed folder in wrong position and added the helloworld assignment in fundmentals

---
+++ PythonStack/_python/python_fundamentals/Hello_World/hello_world.py
@@ -0,0 +1,25 @@
+# 1)print("Hello World")
+name="Noelle"
+favnum = 8
+food1= "Noodles"
+food2= "Pizza"
+# 2a)
+print( "Hello" , name  )
+# 2b)
+print( "Hello " + name  )
+# 3a)
+print( "Hello" , favnum)
+# 3b)
+print( "Hello " + str(favnum))
+# 4a)
+print("I love to eat {} and {}".format(food1,food2))
+#4b)
+print(f"I love to eat {food1} and {food2}")
+
+# bonus
+
+print(f"I love to eat {food1.upper()} and {food2.startswith('P')}")
+# str()
+# parseInt
+# endswith
+# print("I love to eat %s and %s" % (food1 , food2))
