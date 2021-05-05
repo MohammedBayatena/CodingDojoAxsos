f50d7ca0219ec878903ff21cbe7b1567747940c5
Mohammed Bayatena <www.cars2012@gmail.com>
2021-05-05 22:57:20 +0300

    Added loopsbasic1 solution

---
+++ PythonStack/_python/python_fundamentals/forloop_basic1/forLoopBasics1.py
@@ -0,0 +1,50 @@
+#1 Integers 0 to 150
+#--------------------------------
+# for num in range(151):
+#     print(num)
+#--------------------------------
+
+#2 multiples of 5
+#--------------------------------
+# for num in range(5,1001):
+#     if num % 5 == 0:
+#         print(num)
+#     else:
+#         continue
+#--------------------------------
+#3 Counting
+#--------------------------------
+# for num in range(1,101):
+#     if num % 5 == 0 and not num % 10 == 0:
+#         print("Coding")
+#     elif num % 5 == 0 and num % 10 == 0:
+#         print("Coding Dojo")
+#     else:
+#         print(num)
+#--------------------------------
+#4 whoa.
+#--------------------------------
+# sum=0
+# for num in range(0,500001):
+#     if num % 2 != 0:
+#         sum += int(num)
+#     else:
+#         continue;
+# print("Sum is ",sum)
+#--------------------------------
+#5 CountDown by four.
+#--------------------------------
+# for num in range(2018,0,-4):
+#     print(num)
+#--------------------------------
+#6 flexible Counter
+#--------------------------------
+# lowNum = 2
+# highNum = 9
+# mult =3
+# for num in range(lowNum,highNum+1,1):
+#     if num % mult == 0:
+#         print(num)
+#     else:
+#         continue;
+#--------------------------------
