57492d83f2822ec0ba411cec45115eb1db3e5c37
Mohammed Bayatena <www.cars2012@gmail.com>
2021-05-11 00:01:26 +0300

    Solved Play Ground Assignment

---
+++ PythonStack/flask/playground/playground.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return "Hello This is Root"
+
+
+@app.route('/play')
+def play():
+    return render_template("index.html", numOfBoxes=3, color = "blue")
+
+
+@app.route('/play/<int:x>')
+def playwithlen(x):
+    return render_template("index.html", numOfBoxes=x, color = "blue")
+
+
+@app.route('/play/<int:x>/<string:color>')
+def playwithlenncolor(x,color):
+    return render_template("index.html", numOfBoxes=x, color = color)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
