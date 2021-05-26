9c6bc32cde49ebebe3715fce216f59c5b606ffa5
Mohammed Bayatena <www.cars2012@gmail.com>
2021-05-26 19:27:58 +0300

    Added form validations and password encryption

--- PythonStack/django/django_orm/login_register_proj/login_register_app/models.py
+++ PythonStack/django/django_orm/login_register_proj/login_register_app/models.py
@@ -1,7 +1,30 @@
+import re
+
 from django.db import models
-import bcrypt
+
 
 # Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['firstname']) < 2:
+            errors["firstname"] = "First name should be at least 2 characters"
+
+        if len(postData['lastname']) < 2:
+            errors["lastname"] = "Last name should be at least 2 characters"
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):  # test whether a field matches the pattern
+            errors['email'] = "Invalid email address!"
+
+        if len(postData['password']) < 8:
+            errors["passwordlen"] = "Password should be at least 8 characters"
+
+        if postData['password'] != postData['confirmpassword']:
+            errors["passwordmatch"] = "Password and Password confirmation don't match! "
+        return errors
+
 
 class User(models.Model):
     first_name = models.CharField(max_length=200)
@@ -11,6 +34,7 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # cars =
+    objects = UserManager()  # add this line!
 
 
 class Car(models.Model):
--- PythonStack/django/django_orm/login_register_proj/login_register_app/views.py
+++ PythonStack/django/django_orm/login_register_proj/login_register_app/views.py
@@ -1,7 +1,9 @@
+import bcrypt
+from django.contrib import messages
 from django.shortcuts import render, redirect
 
 from . import models
-import bcrypt
+from .models import User
 
 
 # Create your views here.
@@ -15,32 +17,52 @@ def root(request):
 def login(request):
     if request.method == 'POST':
         email = request.POST['emaillogin']
-        password = request.POST['passwordlogin']
         user = models.usercheckexist(email)
-        if user is not None and bcrypt.checkpw(password.encode(),user.password.encode()):
+        errors = {}
+        if user is None:
+            errors['login'] = "User with similar email does not exist!"
+        if len(errors) > 0:
+            # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+            for key, value in errors.items():
+                messages.error(request, value)
+            # redirect the user back to the form to fix the errors
+            return redirect('/')
+        else:
+            password = request.POST['passwordlogin']
+        if user is not None and bcrypt.checkpw(password.encode(), user.password.encode()):
             request.session['id'] = user.id
             request.session['first_name'] = user.first_name
             request.session['last_name'] = user.last_name
-        return redirect('/welcome')
+            messages.success(request, 'Successfully Logged In!')
+            return redirect('/welcome')
+        else:
+            messages.error(request, "Invalid password or email!")
     return redirect('/')
 
 
 def register(request):
     if request.method == "POST":
-        firstname = request.POST['firstname']
-        lastanme = request.POST['lastname']
-        email = request.POST['email']
-        password = request.POST['password']
-        passwordc = request.POST['confirmpassword']
-
-        if password == passwordc:
-            hashed_password = bcrypt.hashpw(password.encode(),bcrypt.gensalt()).decode()
-            user = models.create_user(firstname, lastanme, email, hashed_password)
-            request.session['id'] = user.id
-            request.session['first_name'] = user.first_name
-            request.session['last_name'] = user.last_name
-
-        return redirect('/welcome')
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+            for key, value in errors.items():
+                messages.error(request, value)
+            # redirect the user back to the form to fix the errors
+            return redirect('/')
+        else:
+            firstname = request.POST['firstname']
+            lastanme = request.POST['lastname']
+            email = request.POST['email']
+            password = request.POST['password']
+            passwordc = request.POST['confirmpassword']
+            if password == passwordc:
+                hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+                user = models.create_user(firstname, lastanme, email, hashed_password)
+                request.session['id'] = user.id
+                request.session['first_name'] = user.first_name
+                request.session['last_name'] = user.last_name
+                messages.success(request, 'Successfully Registered!')
+                return redirect('/welcome')
     return redirect('/')
 
 
