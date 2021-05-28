9953f7347990d6d90b246489345f1132f66883d0
Mohammed Bayatena <www.cars2012@gmail.com>
2021-05-28 15:43:18 +0300

    Added favourite books assignment

---
+++ PythonStack/django/django_fullstack/favourite_books_proj/favourite_books_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FavouriteBooksAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'favourite_books_app'
--- PythonStack/django/django_fullstack/favourite_books_proj/favourite_books_proj/settings.py
+++ PythonStack/django/django_fullstack/favourite_books_proj/favourite_books_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y#pfgxosy(fnk+nl*$!v+yixh(^%2)4u05nrha_efhl0tiu*(!'
+SECRET_KEY = 'django-insecure-5uzuw1z_6q0-c)m=gj#v!mxxp5si62k2d*kc5jt(7b@h&_3@ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
