4efd8e7a89df966946f1f94f10dfb86ae50c2b96
Mohammed Bayatena <www.cars2012@gmail.com>
2021-06-08 23:33:34 +0300

    added few stuff

--- Java/JavaFun/src/JavaFun.java
+++ Java/JavaFun/src/JavaFun.java
@@ -1,9 +1,9 @@
 public class JavaFun {
 
     public static void main(String[] args) {
-        System.out.println("My name is Coding Dojo");
-        System.out.println("I am 100 years old");
-        System.out.println("My hometown is Burbank, CA");
+        System.out.println("My name is Mohammed Bayatena");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Berzeit, Palestine");
     }
 }
 
