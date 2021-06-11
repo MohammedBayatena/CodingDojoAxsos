58b46922158a3315b793cbc58d3a88dfb02f1d8d
Mohammed Bayatena <www.cars2012@gmail.com>
2021-06-11 17:05:03 +0300

    Added CalculatorII Assignment (Beans and Rules)

--- Java/OOP/calculator/Calculator.java
+++ Java/OOP/calculator/Calculator.java
@@ -1,16 +1,20 @@
 package OOP.calculator;
 
-public class Calculator {
+import java.util.Stack;
 
+public class Calculator {
     private char operation;
     private double operand1, operand2;
     private double result;
+    private Stack<Object> numberStack = new Stack<>();
+    private Stack<Object> operatorStack = new Stack<>();
 
     public double getResult() {
         return result;
     }
 
     public Calculator() {
+        this.result = 0;
     }
 
     public char getOperation() {
@@ -46,10 +50,57 @@ public class Calculator {
             case '-':
                 result = getOperand1() - getOperand2();
                 break;
+            case '*':
+                result = getOperand1() * getOperand2();
+                break;
+            case '/':
+                result = getOperand1() / getOperand2();
+                break;
             default:
                 System.out.println("Incorrect Operation Entered or Operation Unsupported Yet!");
         }
     }
+
+
+    public void performOperation(char operation) {
+        switch (operation) {
+            case '+':
+                operatorStack.push(operation);
+                break;
+            case '-':
+                operatorStack.push(operation);
+                break;
+            case '*':
+                operatorStack.push(operation);
+                break;
+            case '/':
+                operatorStack.push(operation);
+                break;
+            case '=':
+                result = doOperations();
+                break;
+            default:
+                System.out.println("Incorrect Operation Entered or Operation Unsupported Yet!");
+        }
+    }
+
+    public void performOperation(double operand) {
+        numberStack.push((Double) operand);
+    }
+
+    private double doOperations() {
+        while (!operatorStack.isEmpty()) {
+            if (numberStack.size() >= 2) {
+                operand2 = (double) numberStack.pop();
+                operand1 = (double) numberStack.pop();
+                operation = (char) operatorStack.pop();
+                performOperation();
+                numberStack.push(getResult());
+            }
+        }
+        return result;
+    }
+
 
     public void getResults() {
         System.out.println("Result is: " + getResult());
--- Java/OOP/calculator/CalculatorTesting.java
+++ Java/OOP/calculator/CalculatorTesting.java
@@ -3,12 +3,37 @@ package OOP.calculator;
 public class CalculatorTesting {
 
     public static void main(String[] args) {
+
+        //Simple Calculator I
         Calculator calculator = new Calculator();
         calculator.setOperand1(10.5);
         calculator.setOperation('+'); // Please Enter Operation as Character between '' and not as a String !!
         calculator.setOperand2(5.2);
         calculator.performOperation();
         calculator.getResults();
+
+        //Simple Calculator II
+        //Test1
+        Calculator calculator2 = new Calculator();
+        calculator2.performOperation(10.5);
+        calculator2.performOperation('+'); // Please Enter Operation as Character between '' and not as a String !!
+        calculator2.performOperation(5.2);
+        calculator2.performOperation('*');
+        calculator2.performOperation(10);
+        calculator2.performOperation('=');
+        calculator2.getResults();
+        //Test2
+        calculator2.performOperation(10.5);
+        calculator2.performOperation('+'); // Please Enter Operation as Character between '' and not as a String !!
+        calculator2.performOperation(5.2);
+        calculator2.performOperation('+');
+        calculator2.performOperation(10);
+        calculator2.performOperation('*');
+        calculator2.performOperation(20);
+        calculator2.performOperation('/');
+        calculator2.performOperation(5);
+        calculator2.performOperation('=');
+        calculator2.getResults();
     }
 
 
