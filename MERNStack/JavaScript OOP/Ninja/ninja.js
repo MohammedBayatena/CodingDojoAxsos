47a375c1a93c52b3eb2a7a7cebeab1a2f147bda6
Mohammed Bayatena <www.cars2012@gmail.com>
2021-07-12 19:46:15 +0300

    Added SuperNinja and Sensei Class

--- MERNStack/JavaScript OOP/Ninja/ninja.js
+++ MERNStack/JavaScript OOP/Ninja/ninja.js
@@ -1,24 +1,43 @@
-class Ninja{
-    constructor(name , health , speed = 3 , strength = 3){
+class Ninja {
+    constructor(name, health, speed = 3, strength = 3) {
         this.name = name;
         this.health = health;
         this.speed = speed;
         this.strength = strength;
     }
 
-    sayName(){
-        console.log("My Name is :" + this.name );
+    sayName() {
+        console.log("My Name is :" + this.name);
     }
-    showStats(){
-        console.log(`{ Health: ${this.health} , Speed = ${this.speed} , Strength = ${this.strength} }`);
+
+    showStats() {
+        console.log(`{ Name: ${this.name}, Health: ${this.health} , Speed = ${this.speed} , Strength = ${this.strength} }`);
     }
-    drinkSake(){
-        this.health+=10;
+
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name, wisdom = 10) {
+        super(name, 200, 10, 10);
+        this.wisdom = wisdom;
+    }
+    speakWisdom() {
+        this.drinkSake();
+        console.log(`${this.name} Says: `+"What one programmer can do in one month, two programmers can do in two months.")
     }
 
 }
 
-const ninja1 = new Ninja("Hanabusa",100);
+
+const ninja1 = new Ninja("Hanabusa", 100);
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
+
