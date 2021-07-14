ef0b76df929733ba8705d85f19bfb9aae767782c
Mohammed Bayatena <www.cars2012@gmail.com>
2021-07-14 12:05:09 +0300

    Added ProbItUp assignment

--- MERNStack/React/first_react_proj/src/App.js
+++ MERNStack/React/first_react_proj/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from "./Components/PersonCard";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <PersonCard firstname={"Jane"} lastname={"Doe"} age={45} haircolor={"black"}/>
+            <PersonCard firstname={"Jhon"} lastname={"Smith"} age={88} haircolor={"Brown"}/>
+            <PersonCard firstname={"Millard"} lastname={"Fillmore"} age={50} haircolor={"Brown"}/>
+            <PersonCard firstname={"Maria"} lastname={"Smith"} age={62} haircolor={"Brown"}/>
         </div>
     );
 }
 
 export default App;
+
+
+
+
