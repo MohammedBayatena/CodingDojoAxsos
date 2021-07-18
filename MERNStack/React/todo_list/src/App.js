cb6002e112834335d2974223cac112bb5c1ac3c1
Mohammed Bayatena <www.cars2012@gmail.com>
2021-07-18 20:25:05 +0300

    Added Persistent State so that list is not gone after update

--- MERNStack/React/todo_list/src/App.js
+++ MERNStack/React/todo_list/src/App.js
@@ -8,10 +8,17 @@ export default function App() {
 
     const [tasks, setTasks] = useState([])
 
+    //Initialize the taskslist Variable , Read Tasks from the json file or initialize to an empty Array []
     useEffect(() => {
-        // setTasks(tasks);
-        console.log(tasks)
-    });
+        if (localStorage.getItem("tasksList")) {
+            setTasks(JSON.parse(localStorage.getItem("tasksList")))
+        }
+    }, [])
+
+    // Every Time the Tasks list changes this code excutes
+    useEffect(() => {
+        localStorage.setItem("tasksList", JSON.stringify(tasks))
+    }, [tasks])
 
     const addTask = (task) => {
         setTasks([...tasks, task]);
